6f6f73ecb0c7a828c516614b92930edd19004eb6
ksog66 <ksog66@gmail.com>
2021-02-23 20:32:58 +0530

    feed update

--- app/src/main/java/io/realworld/android/ui/feed/FeedFragment.kt
+++ app/src/main/java/io/realworld/android/ui/feed/FeedFragment.kt
@@ -6,11 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import io.realworld.android.R
 import io.realworld.android.databinding.FragmentFeedBinding
 
-class HomeFragment : Fragment() {
+class FeedFragment : Fragment() {
 
     private lateinit var homeViewModel: FeedViewModel
     private var _binding: FragmentFeedBinding? = null
@@ -18,7 +21,7 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
+    private var navController: NavController? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -30,16 +33,34 @@ class HomeFragment : Fragment() {
         _binding = FragmentFeedBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+
         return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        TabLayoutMediator(binding.tabLayout,binding.viewPager){ tab, position ->
-            binding.viewPager.setCurrentItem(R.layout.fragment_article)
+        navController=binding.let{ Navigation.findNavController(it.root.findViewById(R.id.navigation_feed))}
+        binding.feedTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when(tab?.position){
+                    0->{
+                        navController?.navigate(R.id.gotoGlobalFeedFragment)
+                    }
+                    1->{
+                        navController?.navigate(R.id.gotoMyFeedFragment)
+                    }
+                }
+            }
 
-        }.attach()
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
     }
 
     override fun onDestroyView() {
